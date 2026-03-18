bb24d9f334b3fe58182477ecc6633742bf96ea2c
delairec <celia.delaire+github@mailfence.com>
2026-03-18 23:51:07 +0100

    feat(ui-save-manager): format number with unit symbols

--- packages/util-mapping/presentation/formatters/formatNumber.spec.ts
+++ packages/util-mapping/presentation/formatters/formatNumber.spec.ts
@@ -1,14 +1,63 @@
 import { describe, it, expect} from 'bun:test';
+import {formatNumber} from "./formatNumber";
 
 describe('formatNumber', () => {
-  it('should format number with thousands separator', () => {
+
+  it.each([
+    {symbol:'n', value: 0.000_000_001},
+    {symbol:'µ', value: 0.000_001},
+    {symbol:'k', value: 1000},
+    {symbol:'M', value: 1000_000},
+    {symbol:'G', value: 1000_000_000},
+    {symbol:'G', value: 1000_000_000},
+    {symbol:'T', value: 1000_000_000_000},
+    {symbol:'P', value: 1000_000_000_000_000},
+    {symbol:'E', value: 1000_000_000_000_000_000n},
+    {symbol:'Z', value: 1000_000_000_000_000_000_000n},
+    {symbol:'Y', value: 1000_000_000_000_000_000_000_000n},
+  ])('should format number with $symbol unit symbol', ({symbol, value}) => {
     // Arrange
     const {formatNumber} = require('./formatNumber');
 
     // Act
-    const result = formatNumber(9876543210);
+    const result = formatNumber(value);
 
     // Assert
-    expect(result).toBe('9,876,543,210');
+    expect(result).toBe(`1${symbol}`);
+  });
+
+  it('should handle big values that are not bigint type', () => {
+    // Arrange
+    const {formatNumber} = require('./formatNumber');
+
+    // Act
+    const result = formatNumber(1_987_487_654_321_885);
+
+    // Assert
+    expect(result).toBe('1P');
+  });
+
+  it('should handle non integer numbers', () => {
+    // Arrange
+    const {formatNumber} = require('./formatNumber');
+
+    // Act
+    const result = formatNumber(3210.52039);
+
+    // Assert
+    expect(result).toBe('3.211k');
+  });
+
+  describe('When number is between 0.001 and 999', () => {
+    it.each([0.001, 999, 439])('should format the number %p without symbol', (number) => {
+      // Arrange
+      const {formatNumber} = require('./formatNumber');
+
+      // Act
+      const result = formatNumber(number);
+
+      // Assert
+      expect(result).toBe(`${number}`);
+    });
   });
 });
--- packages/util-mapping/presentation/formatters/formatNumber.ts
+++ packages/util-mapping/presentation/formatters/formatNumber.ts
@@ -1,3 +1,67 @@
-export function formatNumber(num:number){
-  return new Intl.NumberFormat().format(num);
+interface Threshold {
+  value: number | bigint;
+  suffix: string;
+  big?: boolean;
+  multiply?: number;
+}
+
+const thresholds: Threshold[] = [
+  { value: 1_000_000_000_000_000_000_000_000n, suffix: "Y", big: true },
+  { value: 1_000_000_000_000_000_000_000n, suffix: "Z", big: true },
+  { value: 1_000_000_000_000_000_000n, suffix: "E", big: true },
+  { value: 1_000_000_000_000_000, suffix: "P" },
+  { value: 1_000_000_000_000, suffix: "T" },
+  { value: 1_000_000_000, suffix: "G" },
+  { value: 1_000_000, suffix: "M" },
+  { value: 1_000, suffix: "k" },
+  { value: 1, suffix: "" },
+  { value: 0.001, suffix: "" },
+  { value: 0.000_001, suffix: "µ", multiply: 1_000_000 },
+  { value: 0.000_000_001, suffix: "n", multiply: 1_000_000_000 },
+];
+
+export function formatNumber(num: number) {
+  for (const threshold of thresholds) {
+    if(isBigInteger(num, threshold)){
+      const quotient = BigInt(num) / BigInt(threshold.value);
+      const result = Number(quotient);
+      return formatNumberWithSymbol(result, threshold);
+    }
+
+    if(isNumber(num, threshold) && !isBigIntType(threshold.value)){
+      if(!threshold.suffix){
+        return formatNumberWithoutSymbol(num);
+      }
+      if(threshold.multiply){
+        const result = num * threshold.multiply;
+        return formatNumberWithSymbol(result, threshold);
+      }
+
+      const result = num / threshold.value;
+      return formatNumberWithSymbol(result, threshold);
+    }
+  }
+
+  return formatNumberWithoutSymbol(num);
+}
+
+function formatNumberWithoutSymbol(value: number) {
+  return new Intl.NumberFormat().format(value);
+}
+
+function formatNumberWithSymbol(value: number, t:Threshold) {
+  return `${(formatNumberWithoutSymbol(value))}${t.suffix}`;
+}
+
+function isBigIntType(num: number | bigint): num is bigint {
+  return typeof num === 'bigint';
+}
+
+function isBigInteger(num: number, threshold: Threshold) {
+  const isBig = isBigIntType(num) || Number(num) > 999_999_999_999_999;
+  return isBig && BigInt(num) >= threshold.value;
+}
+
+function isNumber(num: number, threshold: Threshold) {
+  return !isBigIntType(num) && num >= threshold.value;
 }
