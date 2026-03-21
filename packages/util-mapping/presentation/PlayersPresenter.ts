458d25e6c4910af61d3e46de1e9ef8cfea48a8cc
delairec <celia.delaire+github@mailfence.com>
2026-03-21 13:54:29 +0100

    feat(ui-save-manager): map world objects ids into readable names

--- packages/util-mapping/presentation/PlayersPresenter.ts
+++ packages/util-mapping/presentation/PlayersPresenter.ts
@@ -1,6 +1,7 @@
 import {PlayersViewModel} from './viewModels/PlayersViewModel';
 import {PlayersPresenterPort} from '../application/ports/PlayersPresenterPort';
 import {PlayerEntity} from "../domain/entities/PlayerEntity";
+import {worldObjectIds} from "./idMapping/worldObjectIds";
 
 export class PlayersPresenter implements PlayersPresenterPort {
   viewModel: PlayersViewModel;
@@ -17,13 +18,22 @@ export class PlayersPresenter implements PlayersPresenterPort {
       columns: [
         {
           header: 'Equipment',
-          values: player.equipment
+          values: this.mapListWithEmptyMessage(player.equipment, '(/) No equipment'),
         },
         {
           header: 'Inventory',
-          values: player.inventory
+          values: this.mapListWithEmptyMessage(player.inventory, '(/) No items'),
         }
       ]
     }));
+  }
+
+  private mapItemIdToItemName(itemId: string) {
+    const worldObjectName: string = worldObjectIds[itemId];
+    return worldObjectName ?? `Unknown Item (${itemId})`;
+  }
+
+  private mapListWithEmptyMessage(list: string[], message: string) {
+    return list.length === 0 ? [message] : list.map(this.mapItemIdToItemName);
   }
 }
