cf5eb83137ff03c964c5cf2e1abaad81996d0445
delairec <celia.delaire+github@mailfence.com>
2026-03-22 11:39:40 +0100

    feat(ui-save-manager): handle multiple strategies when formatting numbers

--- packages/util-mapping/presentation/formatters/formatNumber.spec.ts
+++ packages/util-mapping/presentation/formatters/formatNumber.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect} from 'bun:test';
-import {formatNumber} from "./formatNumber";
+import {formatNumber, FormatNumberStrategies} from "./formatNumber";
 
 describe('formatNumber', () => {
 
@@ -16,33 +16,24 @@ describe('formatNumber', () => {
     {symbol:'Z', value: 1000_000_000_000_000_000_000n},
     {symbol:'Y', value: 1000_000_000_000_000_000_000_000n},
   ])('should format number with $symbol unit symbol', ({symbol, value}) => {
-    // Arrange
-    const {formatNumber} = require('./formatNumber');
-
     // Act
-    const result = formatNumber(value);
+    const result = formatNumber(value, FormatNumberStrategies.SYMBOL);
 
     // Assert
     expect(result).toBe(`1${symbol}`);
   });
 
   it('should handle big values that are not bigint type', () => {
-    // Arrange
-    const {formatNumber} = require('./formatNumber');
-
     // Act
-    const result = formatNumber(1_987_487_654_321_885);
+    const result = formatNumber(1_987_487_654_321_885, FormatNumberStrategies.SYMBOL);
 
     // Assert
-    expect(result).toBe('1P');
+    expect(result).toBe('1.987P');
   });
 
   it('should handle non integer numbers', () => {
-    // Arrange
-    const {formatNumber} = require('./formatNumber');
-
     // Act
-    const result = formatNumber(3210.52039);
+    const result = formatNumber(3210.52039, FormatNumberStrategies.SYMBOL);
 
     // Assert
     expect(result).toBe('3.211k');
@@ -50,14 +41,31 @@ describe('formatNumber', () => {
 
   describe('When number is between 0.001 and 999', () => {
     it.each([0.001, 999, 439])('should format the number %p without symbol', (number) => {
-      // Arrange
-      const {formatNumber} = require('./formatNumber');
-
       // Act
-      const result = formatNumber(number);
+      const result = formatNumber(number, FormatNumberStrategies.SYMBOL);
 
       // Assert
       expect(result).toBe(`${number}`);
+    });
+  });
+
+  describe('When formatting strategy is THOUSANDS_SEPARATOR', () => {
+    it('should format the number with thousands separator', () => {
+      // Act
+      const result = formatNumber(1_234_567, FormatNumberStrategies.THOUSANDS_SEPARATOR);
+
+      // Assert
+      expect(result).toBe('1,234,567');
+    });
+  });
+
+  describe('When formatting strategy is not provided', () => {
+    it('should default to thousands separator', () => {
+      // Act
+      const result = formatNumber(1_234_567, FormatNumberStrategies.THOUSANDS_SEPARATOR);
+
+      // Assert
+      expect(result).toBe('1,234,567');
     });
   });
 });
