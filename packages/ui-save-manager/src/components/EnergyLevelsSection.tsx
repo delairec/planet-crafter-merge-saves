028bf650d2ad5a2930e6ddd49b7a6858116d2931
delairec <celia.delaire+github@mailfence.com>
2026-03-28 22:44:52 +0100

    feat(ui-save-manager): display power section

---
+++ packages/ui-save-manager/src/components/EnergyLevelsSection.tsx
@@ -0,0 +1,30 @@
+import {Accessor, createEffect, createSignal} from "solid-js";
+import FieldsGroup from "./structure/FieldsGroup";
+import {EnergyLevelsViewModel} from "../../../util-mapping/presentation/viewModels/EnergyLevelsViewModel";
+import {ParsedSections} from "../../../util-types/gameDefinitions";
+import {LoadEnergyLevelsSectionController} from "../../../util-mapping/controllers/LoadEnergyLevelsSectionController";
+
+interface EnergyLevelsProps {
+  sections: Accessor<ParsedSections>;
+}
+
+export default function EnergyLevelsSection({sections}: EnergyLevelsProps) {
+  const [energyLevelsColumns, setEnergyLevelsColumns] = createSignal<EnergyLevelsViewModel['energyLevels']['columns']>([]);
+  const [title, setTitle] = createSignal<string | null>(null);
+
+  createEffect(() => {
+    const {energyLevels} = LoadEnergyLevelsSectionController.loadEnergyLevelsSection(sections());
+    setEnergyLevelsColumns(energyLevels.columns);
+    setTitle(title);
+  });
+
+  return (
+    <div>
+      <h3>Power</h3>
+      <div class="fields-group-container">
+        <FieldsGroup columns={energyLevelsColumns}/>
+      </div>
+    </div>
+  )
+    ;
+}
--- packages/util-mapping/controllers/LoadEnergyLevelsSectionController.spec.ts
+++ packages/util-mapping/controllers/LoadEnergyLevelsSectionController.spec.ts
@@ -2,25 +2,9 @@ import {describe, expect, it} from 'bun:test';
 import {EnergyLevelsViewModel} from "../presentation/viewModels/EnergyLevelsViewModel";
 import {parseSaveSections} from "../../util-parsing/parseSaveSections";
 import {createFakeSaveContent} from "../../util-testing/fixtures/createFakeSaveContent";
-import {ParsedSections} from "../../util-types/gameDefinitions";
-import {LoadEnergyLevelsSection} from "../application/LoadEnergyLevelsSection";
-import {SaveSectionsReaderService} from "../infrastructure/SaveSectionsReaderService";
-import {EnergyLevelsPresenter} from "../presentation/EnergyLevelsPresenter";
+import {LoadEnergyLevelsSectionController} from "./LoadEnergyLevelsSectionController";
 
 const nbsp = '\u00A0';
-
-class LoadEnergyLevelsSectionController {
-
-  static loadEnergyLevelsSection(sections: ParsedSections): EnergyLevelsViewModel {
-    const saveParser = new SaveSectionsReaderService(sections);
-    const presenter = new EnergyLevelsPresenter();
-    const useCase = new LoadEnergyLevelsSection(saveParser, presenter);
-
-    useCase.execute();
-
-    return presenter.viewModel;
-  }
-}
 
 describe('LoadEnergyLevelsSectionController', () => {
   it('should present computed energy levels from parsed save', () => {
